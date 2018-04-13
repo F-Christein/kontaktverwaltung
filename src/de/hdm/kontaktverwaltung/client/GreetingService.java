f52e2294de7ad09854cd3a73d93a51f291350e3b
kim <kim@kims-mbp.fritz.box>
2018-04-13 15:33:03 +0200

    Testcommit

--- src/de/hdm/kontaktverwaltung/client/GreetingService.java
+++ src/de/hdm/kontaktverwaltung/client/GreetingService.java
@@ -2,7 +2,7 @@ package de.hdm.kontaktverwaltung.client;
 
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
-
+// Test
 /**
  * The client-side stub for the RPC service.
  */
